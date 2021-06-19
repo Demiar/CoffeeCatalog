800b509a8c001915e1c8f2afd257621426d194d9
BelyaevAlexey <alexey174@gmail.com>
2021-06-19 13:35:29 +0300

    Сделал launchScreen, экран ввода логина и пароля

--- CoffeeCatalog/logInViewController.swift
+++ CoffeeCatalog/logInViewController.swift
@@ -8,12 +8,80 @@
 import UIKit
 
 class logInViewController: UIViewController {
-
+    @IBOutlet weak var loginTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    @IBOutlet weak var logInButton: UIButton!
+    
+    let user = User(login: "user", password: "password")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loginTF.delegate = self
+        passwordTF.delegate = self
+        self.view.addBackground()
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
     }
 
+    @IBAction func logInButton(_ sender: UIButton) {
+        if !verification() {
+            alertMessage(title: "Ошибка", message: "Логин или пароль не правильный!")
+        }
+    }
+    @IBAction func forgotPassword(_ sender: UIButton) {
+        alertMessage(title: "Забыли логин или пароль?", message: "Логин - \(user.login) , Пароль - \(user.password)")
+    }
+    
+    private func verification() -> Bool {
+        if loginTF.text == user.login && passwordTF.text == user.password {
+            return true
+        }
+        return false
+    }
+    
+    private func alertMessage(title: String, message: String, buttonTitle: String = "Ok") {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
 
+        alert.addAction(UIAlertAction(title: buttonTitle, style: UIAlertAction.Style.default, handler: nil))
+
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 }
+
+extension logInViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        let nextTag = textField.tag + 1
+            
+        if let nextResponder = textField.superview?.viewWithTag(nextTag) {
+            nextResponder.becomeFirstResponder()
+        } else {
+            textField.resignFirstResponder()
+            logInButton.sendActions(for: .touchUpInside)
+        }
+        
+        return true
+    }
+}
+
+extension UIView {
+func addBackground() {
+    // screen width and height:
+    let width = UIScreen.main.bounds.size.width
+    let height = UIScreen.main.bounds.size.height
+
+    let imageViewBackground = UIImageView(frame: CGRect(x: 0, y: 0, width: width, height: height))
+    imageViewBackground.image = UIImage(named: "wallpaper.jpg")
+
+    // you can change the content mode:
+    imageViewBackground.contentMode = UIView.ContentMode.scaleAspectFill
+
+    self.addSubview(imageViewBackground)
+    self.sendSubviewToBack(imageViewBackground)
+}}
 
